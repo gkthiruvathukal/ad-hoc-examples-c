f02619d9c1bd2a2192000b071424942941303ebb
George K. Thiruvathukal <gkt@cs.luc.edu>
2020-09-03 13:22:25 -0500

    simple example of regex to get word matches

--- src/getline_demo.c
+++ src/getline_demo.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <regex.h>
 
+#define WITH_LINUX
+#define MAX_ERROR_MSG 80
 /*
  * This example shows a strbuffer_t-enabled getline() like function
  * and how to use strtok() to separate text with whitespace delimiters.
@@ -11,18 +14,54 @@
 
 #define DELIMS " \n\r\t"
 
+
+int compile_regex (regex_t * r, const char * regex_text)
+{
+    //int status = regcomp (r, regex_text, REG_EXTENDED|REG_ENHANCED|REG_NEWLINE);
+    //
+#ifdef WITH_APPLE
+    int status = regcomp (r, regex_text, REG_EXTENDED|REG_ENHANCED|REG_NEWLINE);
+#endif
+#ifdef WITH_LINUX
+    int status = regcomp (r, regex_text, REG_EXTENDED|REG_NEWLINE);
+#endif
+    if (status != 0) {
+        char error_message[MAX_ERROR_MSG];
+        regerror (status, r, error_message, MAX_ERROR_MSG);
+        printf ("Regex error compiling '%s': %s\n", regex_text, error_message);
+        return 1;
+    }
+    return 0;
+}
+
+void print_words (regex_t *r, char * to_match, int max_matches)
+{
+    char * p = to_match;
+    const int n_matches = 1;
+    regmatch_t m[n_matches];
+
+    for(;;) {
+        int nomatch = regexec (r, p, n_matches, m, 0);
+        if (nomatch) break;
+        for (int i = 0; i < n_matches; i++) {
+            char* word = strndup(p+m[i].rm_so, m[i].rm_eo - m[i].rm_so);
+            printf("Match at start %d, end %d, word %s\n", m[i].rm_so, m[i].rm_eo, word);
+        }
+        p += m[0].rm_eo;
+    }
+}
+
 int main(int argc, char* argv[]) {
+    
     strbuffer_t buffer;
     strbuffer_init(&buffer, 80, 10);
+    regex_t r;
+    compile_regex(&r, "(\\w+)");
     for (;;) {
         int eof;
         char* line = strbuffer_getline(&buffer, &eof);
         if (eof) break;
-        char* next_word = strtok(line, DELIMS);
-        while (next_word != NULL) {
-            printf("word: %s\n", next_word);
-            next_word = strtok(NULL, DELIMS);
-        }
+        print_words(&r, line, 100);
         free(line);
     }
 }
